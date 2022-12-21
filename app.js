c7aeea9046947f3ae3e4dd6b9903f8a08180b580
Deepakpulukanti <113703393+Deepakpilikanti@users.noreply.github.com>
2022-12-21 13:18:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+const path = require("path");
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodo();
+  if (request.accepts("html")) {
+    response.render("index", { allTodos });
+  } else {
+    response.json({ allTodos });
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/todos", async (request, response) => {
+  const todoItems = await Todo.getTodo();
+  response.json(todoItems);
+});
+
+app.post("/todos", async (request, response) => {
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to update a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async (request, response) => {
+  console.log("Delete a todo by ID:", request.params.id);
+  try {
+    const deletedTodo = await Todo.deleteTodo(request.params.id);
+    return response.send(deletedTodo ? true : false);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+module.exports = app;
